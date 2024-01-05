15e397461f22eabf650b01e6be602d86747672c2
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2024-01-05 15:28:17 +0000

    Installed eslint and stylelint linters

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-
 import './App.css';
 
 function App() {
   return (
     <div className="app">
-     <h1> Welcome to barato one way platform for your shoping joys</h1>
+      <h1> Welcome to barato one way platform for your shoping joys</h1>
     </div>
   );
 }
