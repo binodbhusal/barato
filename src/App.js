df0d833c7f9de0a4505051e1e70c895044d65e67
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2024-01-05 15:11:02 +0000

    Install react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+     <h1> Welcome to barato one way platform for your shoping joys</h1>
+    </div>
+  );
+}
+
+export default App;
