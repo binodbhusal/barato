47156a809dc99126864b64a552835f4bdea58873
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2024-01-12 23:23:30 +0000

    Added home component

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,46 @@
+import './Home.scss';
+import { useEffect, useContext, useCallback } from 'react';
+import Banner from './Banner/Banner';
+import Category from './Category/Category';
+import Products from '../Products/Products';
+import fetchDataFromApi from '../../utils/api';
+import { Context } from '../../utils/context';
+
+const Home = () => {
+  const {
+    categories, setCategories, products, setProducts,
+  } = useContext(Context);
+  const getCategories = useCallback(() => {
+    fetchDataFromApi('/api/categories?populate=*').then((res) => {
+      setCategories(res);
+    });
+  }, [setCategories]);
+
+  const getProducts = useCallback(() => {
+    fetchDataFromApi('/api/products?populate=*')
+      .then((res) => {
+        console.log(res);
+        setProducts(res);
+      });
+  }, [setProducts]);
+  useEffect(() => {
+    getCategories();
+    getProducts();
+  }, [getCategories, getProducts]);
+
+  return (
+
+    <div>
+      <Banner />
+      <div className="main-content">
+        <div className="layout">
+          <Category categories={categories} />
+          <Products products={products} headingText="Popular Products" />
+        </div>
+      </div>
+    </div>
+
+  );
+};
+
+export default Home;
