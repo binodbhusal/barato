1f3eb50158901438eb0f583a8e728d485ec809f5
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2024-01-13 14:19:35 +0000

    add single and related products component

---
+++ src/components/SingleProduct/RelatedProducts/RelatedProducts.jsx
@@ -0,0 +1,14 @@
+import { useEffect } from 'react';
+import Products from '../../Products/Products';
+
+const RelatedProducts = () => {
+  useEffect(() => {
+  });
+  return (
+    <div className="related-products">
+      <Products headingText="Related Products" />
+    </div>
+  );
+};
+
+export default RelatedProducts;
