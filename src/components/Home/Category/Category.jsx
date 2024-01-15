c65f9a32a53ae20e28f6132401207a92b3059496
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2024-01-15 14:43:03 +0000

    adde navigae and method

--- src/components/Home/Category/Category.jsx
+++ src/components/Home/Category/Category.jsx
@@ -1,15 +1,21 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 import './Category.scss';
-import { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { useNavigate } from 'react-router-dom';
 
 const Category = ({ categories }) => {
-  useEffect(() => {
-  });
+  const navigate = useNavigate();
+
+  const handleClick = (item) => {
+    navigate(`category/${item.id}`);
+  };
+
   return (
     <div className="shop-by-category">
       <div className="categories">
         { categories?.data?.map((item) => (
-          <div key={item.id} className="category">
+          <div key={item.id} className="category" onClick={() => handleClick(item)}>
             <img
               key={item.id}
               src={process.env.REACT_APP_DEV_URL + (item.attributes.img.data[0]?.attributes.url || '')}
