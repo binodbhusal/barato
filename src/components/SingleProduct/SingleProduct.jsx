df229afe6ef6ed4dcefd02f946cb81efeb802a2f
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2024-01-15 14:33:47 +0000

    Added single product functionality t SingleProduct component

--- src/components/SingleProduct/SingleProduct.jsx
+++ src/components/SingleProduct/SingleProduct.jsx
@@ -1,32 +1,63 @@
-import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useState, useContext } from 'react';
 import './SingleProduct.scss';
 import { FaCartPlus, FaFacebook } from 'react-icons/fa';
 import { FaSquareXTwitter, FaLinkedin, FaInstagram } from 'react-icons/fa6';
 import RelatedProducts from './RelatedProducts/RelatedProducts';
-import prod from '../../assets/products/earbuds-prod-1.webp';
+import useFetch from '../../hooks/useFetch';
+import { Context } from '../../utils/context';
 
 const Singleproduct = () => {
-  useEffect(() => {
+  const { handleAddtoCart } = useContext(Context);
+  const { id } = useParams();
+  const [quantity, setQuantity] = useState(1);
+  const { data } = useFetch(`/api/products?populate=*&[filters][id]=${id}`);
+  if (!data || !data.data || data.data.length === 0) return null;
 
-  });
+  const product = data.data[0].attributes;
+  const handleIncrement = () => setQuantity((prevState) => prevState + 1);
+  const handleDecrement = () => {
+    setQuantity((prevState) => (prevState === 1 ? 1 : prevState - 1));
+  };
   return (
     <div className="single-product-main-content">
       <div className="layout">
         <div className="single-product-page">
           <div className="left">
-            <img src={prod} alt="" />
+            <img
+              src={
+                process.env.REACT_APP_DEV_URL
+                + product.img.data[0].attributes.url
+            }
+              alt=""
+            />
           </div>
           <div className="right">
-            <span className="name">Product Name</span>
-            <span className="price">Price</span>
-            <span className="description">Description</span>
+            <span className="name">{product.title}</span>
+            <span className="price">
+              {' '}
+              &euro;
+              {' '}
+              {product.price}
+            </span>
+            <span className="description">{product.Description}</span>
             <div className="cart-buttons">
               <div className="quantity-buttons">
-                <span>-</span>
-                <span>5</span>
-                <span>+</span>
+                <span>
+                  {' '}
+                  <button type="button" className="btn-incre" onClick={handleDecrement}>-</button>
+                </span>
+                <span>{quantity}</span>
+                <span><button className="btn-incre" type="button" onClick={handleIncrement}>+</button></span>
               </div>
-              <button className="btn-add-to-cart" type="button">
+              <button
+                className="btn-add-to-cart"
+                type="button"
+                onClick={() => {
+                  handleAddtoCart(data.data[0], quantity);
+                  setQuantity(1);
+                }}
+              >
                 <FaCartPlus size={20} />
                 ADD TO CART
               </button>
@@ -35,8 +66,10 @@ const Singleproduct = () => {
             <div className="info-item">
               <span className="text-bold">
                 Category:
-                <span> Headphones</span>
-
+                <span>
+                  {' '}
+                  {product.categories.data[0].attributes.title}
+                </span>
               </span>
               <span className="text-bold share">
                 Share:
@@ -51,7 +84,10 @@ const Singleproduct = () => {
             </div>
           </div>
         </div>
-        <RelatedProducts />
+        <RelatedProducts
+          productId={id}
+          categoryId={product.categories.data[0].id}
+        />
       </div>
     </div>
 
