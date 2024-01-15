f9173d3d734b4b32dd4d43924e99ec5c6a3f090e
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2024-01-15 15:01:21 +0000

    update cartitem.jsx

--- src/components/Cart/CartItem/CartItem.jsx
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -1,33 +1,50 @@
-import { useEffect } from 'react';
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+import { useContext } from 'react';
 import { MdClose } from 'react-icons/md';
-import prod from '../../../assets/products/earbuds-prod-1.webp';
+import { Context } from '../../../utils/context';
 import './CartItem.scss';
 
 const CartItem = () => {
-  useEffect(() => {
-
-  });
+  const { cartItems, handleRemoveCart, handlecartProductQuantity } = useContext(Context);
   return (
     <div className="cart-products">
-      <div className="cart-product">
-        <div className="img-container">
-          <img src={prod} alt="" />
-        </div>
-        <div className="product-details">
-          <span className="name">Product Name</span>
-          <MdClose className="btn-close" />
-          <div className="quantity-buttons">
-            <span>-</span>
-            <span>5</span>
-            <span>+</span>
+      {cartItems.map((item) => (
+        <div className="cart-product" key={item.id}>
+          <div className="img-container">
+            <img
+              src={
+              process.env.REACT_APP_DEV_URL
+              + item.attributes.img.data[0].attributes.url
+            }
+              alt=""
+            />
           </div>
-          <div className="text">
-            <span>3</span>
-            <span>x</span>
-            <span className="highlight">&euro;49,99</span>
+          <div className="product-details">
+            <span className="name">{item.attributes.title}</span>
+            <MdClose className="btn-close" onClick={() => handleRemoveCart(item)} />
+            <div className="quantity-buttons">
+              <span onClick={() => handlecartProductQuantity('dec', item)}>-</span>
+              <span>{item.attributes.quantity}</span>
+              <span onClick={() => handlecartProductQuantity('inc', item)}>+</span>
+            </div>
+            <div className="text">
+              <span>
+                { item.attributes.quantity === 1 ? '1 item' : `${item.attributes.quantity} items` }
+                {' '}
+                {' '}
+                price
+              </span>
+              <span>=</span>
+              <span className="highlight">
+                &euro;
+                { item.attributes.price * item.attributes.quantity}
+              </span>
+            </div>
           </div>
         </div>
-      </div>
+      ))}
+
     </div>
   );
 };
