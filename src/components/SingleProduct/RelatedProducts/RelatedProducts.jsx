14161d99d0834c6516d763f06bdc39791b5255d3
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2024-01-15 14:40:46 +0000

    update relatedproduct component

--- src/components/SingleProduct/RelatedProducts/RelatedProducts.jsx
+++ src/components/SingleProduct/RelatedProducts/RelatedProducts.jsx
@@ -1,14 +1,24 @@
-import { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import useFetch from '../../../hooks/useFetch';
 import Products from '../../Products/Products';
 
-const RelatedProducts = () => {
-  useEffect(() => {
-  });
+const RelatedProducts = ({ productId, categoryId }) => {
+  const { data } = useFetch(
+    `/api/products?populate=*&filters[id][$ne]=${productId}&filters[categories][id]=${categoryId}&pagination[start]=0&pagination[limit]=4`,
+  );
   return (
     <div className="related-products">
-      <Products headingText="Related Products" />
+      <Products headingText="Related Products" products={data} />
     </div>
   );
 };
 
 export default RelatedProducts;
+RelatedProducts.propTypes = {
+  productId: PropTypes.number,
+  categoryId: PropTypes.number,
+};
+RelatedProducts.defaultProps = {
+  productId: null,
+  categoryId: null,
+};
