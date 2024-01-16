e8fe5b0bfaf557b0ab77cd386b93fbd04f2da47b
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2024-01-16 22:45:58 +0000

    Add slideImageDara.js inlcuded all silde images in an array

---
+++ src/components/Home/Banner/slideImgData.js
@@ -0,0 +1,8 @@
+const slides = [
+
+  { id: 1, path: '../assets/carouselimages/img1.webp' },
+  { id: 2, path: '../assets/carouselimages/img2.webp' },
+  { id: 3, path: '../assets/carouselimages/img3.webp' },
+  { id: 4, path: '../assets/carouselimages/img4.webp' },
+];
+export default slides;
