3e282aee11d915d13cae2b0087f6df3d33e7399b
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2024-01-20 18:15:03 +0000

    update carousel for different small and large screen

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,14 @@
 import './Home.scss';
-import { useEffect, useContext, useCallback } from 'react';
-import Banner from './Banner/Banner';
+import {
+  useEffect, useContext, useCallback, useState,
+} from 'react';
+// import Banner from './Banner/Banner';
 import Category from './Category/Category';
 import Products from '../Products/Products';
 import fetchDataFromApi from '../../utils/api';
 import { Context } from '../../utils/context';
+import MainCarousel from './Banner/MainCarousel';
+import { slidesLg, slidesSm } from './Banner/slideImgData';
 
 const Home = () => {
   const {
@@ -27,14 +31,53 @@ const Home = () => {
     getProducts();
   }, [getCategories, getProducts]);
 
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
   return (
 
     <div>
-      <Banner />
+      <div
+        className="carouselimg-container"
+      >
+        { windowWidth > 768 ? (
+          <MainCarousel autoSlides>
+            {slidesLg.map((s) => (
+              <img
+                src={s.path}
+                key={s.id}
+                alt={`carouselSlide-${s.id}`}
+                style={{ objectFit: 'cover', width: '100%' }}
+
+              />
+            ))}
+          </MainCarousel>
+        ) : (
+          <MainCarousel autoSlides>
+            {slidesSm.map((s) => (
+              <img
+                src={s.path}
+                key={s.id}
+                alt={`carouselSlide-${s.id}`}
+                style={{ objectFit: 'cover', width: '100%' }}
+              />
+            ))}
+          </MainCarousel>
+        )}
+      </div>
       <div className="main-content">
         <div className="layout">
-          <Category categories={categories} />
-          <Products products={products} headingText="Popular Products" />
+          <Category categories={categories} headingText="Shop By Categories" />
+
+          <Products products={products} headingText="Feature Products" />
         </div>
       </div>
     </div>
