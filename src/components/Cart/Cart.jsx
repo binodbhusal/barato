f5e802c69207b599346b6a0e601298af0aac24a3
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2024-01-20 18:21:07 +0000

    include stripe payment method

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,34 +1,83 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useRef } from 'react';
+import { IoIosArrowForward } from 'react-icons/io';
+import { useNavigate } from 'react-router-dom';
+
 import { MdClose } from 'react-icons/md';
 import { BsCartX } from 'react-icons/bs';
 import PropTypes from 'prop-types';
+import { loadStripe } from '@stripe/stripe-js';
 import CartItem from './CartItem/CartItem';
 import './Cart.scss';
 import { Context } from '../../utils/context';
+import { makePaymentRequest } from '../../utils/api';
 
-const Cart = ({ setShowCart }) => {
+const Cart = ({ showCart, setShowCart }) => {
   const { cartItems, cartSubTotal } = useContext(Context);
+  const cartContentRef = useRef(null);
+  const navigate = useNavigate();
+  const handlePayment = async () => {
+    try {
+      const stripePromise = loadStripe(
+        process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY,
+      );
+      const stripe = await stripePromise;
+      const res = await makePaymentRequest.post('/api/orders', {
+        products: cartItems,
+      });
+      await stripe.redirectToCheckout({
+        sessionId: res.data.stripeSession.id,
+      });
+    } catch (error) {
+      throw new Error(error);
+    }
+  };
+
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (cartContentRef.current && cartContentRef.current.contains(event.target)) return;
+      setShowCart(false);
+    };
+    document.addEventListener('mousedown', handleOutsideClick);
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick);
+    };
+  }, [setShowCart]);
+
   return (
     <>
-      <div className="cart-panel">
+      <div className={`cart-panel ${showCart ? '' : 'hide'}`}>
         <div className="opac-layer" />
-        <div className="cart-content">
+        <div className={`cart-content ${showCart ? '' : 'hide'}`} ref={cartContentRef}>
           <div className="cart-header">
-            <span className="heading">Shopping Cart</span>
+            <span className="heading">Your Cart</span>
             <button
               className="close-btn"
               type="button"
-              onClick={() => setShowCart(false)}
+              aria-label="closebtn"
+              onClick={() => {
+                setShowCart(false);
+              }}
             >
               <MdClose />
-              <span className="text">Close</span>
+
             </button>
           </div>
           {!cartItems?.length && (
           <div className="empty-card">
             <BsCartX />
             <span>No Products in the Cart.</span>
-            <button type="button" className="btn-cta">RETURN TO SHOP</button>
+            <button
+              type="button"
+              className="btn-cta"
+              onClick={() => {
+                navigate('/');
+                setShowCart(false);
+              }}
+            >
+              Continue Shopping
+
+            </button>
+
           </div>
           )}
           {!!cartItems?.length && (
@@ -36,7 +85,7 @@ const Cart = ({ setShowCart }) => {
             <CartItem />
             <div className="cart-footer">
               <div className="sub-total">
-                <span className="text">Subtotal:</span>
+                <span className="text">Cart total:</span>
                 <span className="text total">
                   &euro;
                   {' '}
@@ -44,7 +93,18 @@ const Cart = ({ setShowCart }) => {
                 </span>
               </div>
               <div className="button">
-                <button type="button" className="checkout-cta">Checkout</button>
+                <button
+                  type="button"
+                  className="checkout-cta"
+                  onClick={handlePayment}
+                >
+                  <span>
+                    Confirm Order
+                    {'  '}
+                    <IoIosArrowForward />
+                  </span>
+
+                </button>
               </div>
             </div>
           </>
@@ -55,10 +115,12 @@ const Cart = ({ setShowCart }) => {
   );
 };
 Cart.propTypes = {
-  setShowCart: PropTypes.bool,
-
+  setShowCart: PropTypes.func,
+  showCart: PropTypes.bool,
 };
 Cart.defaultProps = {
   setShowCart: false,
+  showCart: false,
+
 };
 export default Cart;
