6423f2b7aaa8b95cb21aeaccd70a82070c53e96e
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2024-01-20 18:07:37 +0000

    Add make payment method

--- src/utils/api.js
+++ src/utils/api.js
@@ -12,7 +12,14 @@ const fetchDataFromApi = async (url) => {
 
     return data;
   } catch (error) {
-    console.log(error);
+    throw new Error(error);
   }
 };
 export default fetchDataFromApi;
+
+export const makePaymentRequest = axios.create({
+  baseURL: process.env.REACT_APP_DEV_URL,
+  headers: {
+    Authorization: `bearer ${process.env.REACT_APP_STRIPE_APP_KEY}`,
+  },
+});
