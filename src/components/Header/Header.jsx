90d78d39aa50815a889923bb6ec86ebcbd9775c8
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2024-01-21 18:04:58 +0000

    Add login functionality with Auth0 and include style

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,7 @@ import { TbSearch } from 'react-icons/tb';
 import { CgShoppingBag } from 'react-icons/cg';
 
 import { IoPersonOutline } from 'react-icons/io5';
+import { useAuth0 } from '@auth0/auth0-react';
 
 import './Header.scss';
 import { useEffect, useState, useContext } from 'react';
@@ -13,11 +14,16 @@ import Search from './Search/Search';
 import { Context } from '../../utils/context';
 
 const Header = () => {
+  const {
+    loginWithRedirect, isAuthenticated, user, logout,
+  } = useAuth0();
+
   const { cartCount } = useContext(Context);
   const navigate = useNavigate();
   const [scrolls, setScrolls] = useState(false);
   const [showCart, setShowCart] = useState(false);
   const [showSearch, setShowSearch] = useState(false);
+  const [showUserInfo, setShowUserInfo] = useState(false);
 
   const handleScroll = () => {
     const offset = window.scrollY;
@@ -32,6 +38,12 @@ const Header = () => {
   });
   const handleClick = () => {
     navigate('/');
+  };
+  const handlelogOut = () => {
+    logout({ returnTo: window.location.origin });
+  };
+  const toggleUserProfile = () => {
+    setShowUserInfo(!showUserInfo);
   };
   return (
     <>
@@ -58,7 +70,42 @@ const Header = () => {
               <TbSearch className="search-icon" onClick={() => setShowSearch(true)} />
             </div>
             <div className="person">
-              <IoPersonOutline className="person" />
+              { isAuthenticated ? (
+                <>
+                  <button type="button" onClick={toggleUserProfile}>
+                    <img src={user.picture} alt={user.name} />
+                  </button>
+                  {showUserInfo && (
+                  <div className="user-info">
+                    <span className="user-info-span">
+                      <img src={user.picture} alt={user.name} />
+                      {user.name}
+                      {' '}
+                      <br />
+                      {user.email}
+                    </span>
+                    <button
+                      type="button"
+                      onClick={handlelogOut}
+                    >
+                      Logout
+
+                    </button>
+                  </div>
+                  )}
+
+                </>
+              ) : (
+                <button onClick={() => loginWithRedirect()} type="button">
+                  <span className="person-span">
+                    <IoPersonOutline />
+                    {' '}
+                    Log in
+                  </span>
+                </button>
+
+              )}
+
             </div>
             <span className="cart-icon">
               <CgShoppingBag onClick={() => setShowCart(true)} />
@@ -66,6 +113,7 @@ const Header = () => {
             </span>
           </div>
         </div>
+
       </header>
       {showCart && <Cart setShowCart={setShowCart} />}
       {showSearch && <Search setShowSearch={setShowSearch} />}
